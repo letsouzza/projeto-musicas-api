f0359d9cdcb00b2902c7f3bce016500ff86c0333
letsouzza <leticia.sa1907@gmail.com>
2025-04-10 15:14:53 -0300

    Novas tabelas

--- database/scriptSQL.sql
+++ database/scriptSQL.sql
@@ -14,6 +14,59 @@ create table tbl_musica (
     link 			varchar(200)
 );
 
+#Criação na Tabela usuário
+create table tbl_usuario (
+	id_usuario 		int not null primary key auto_increment,
+    username 		varchar(100) not null,
+	email    		varchar(100) not null,
+	senha    		varchar(100) not null,
+	foto    		varchar(200)
+);
+
+#Criação da tabela nacionalidade
+create table tbl_nacionalidade (
+	id_nacionalidade 	int not null primary key auto_increment,
+    nacionalidade 		varchar(100) not null
+);
+
+#Criação da Tabela de tipo artista
+create table tbl_tipo_artista (
+	id_tipo_artista 	int not null primary key auto_increment,
+    tipo_artista 		varchar(100) not null
+);
+
+#Criação da Tabela gravadora
+create table tbl_gravadora (
+	id_gravadora 	int not null primary key auto_increment,
+    nome 			varchar(100) not null,
+    telefone		varchar(20) not null
+);
+
+#Criação da Tabela banda
+create table tbl_banda (
+	id_banda 		int not null primary key auto_increment,
+    nome 			varchar(100) not null,
+    data_criacao	date not null
+);
+
+#Criação da Tabela tipo album 
+create table tbl_tipo_album (
+	id_tipo_album   	int not null primary key auto_increment,
+    tipo_album   		varchar(100) not null
+);
+
+#Criação da tabela genero 
+create table tbl_genero (
+	id_genero   	int not null primary key auto_increment,
+    genero   		varchar(100) not null
+);
+
+#Criação da Tabela tipo artista
+create table tbl_tipo_artista (
+	id_tipo_artista   	int not null primary key auto_increment,
+    tipo_artista  		varchar(100) not null
+);
+
 show tables;
 
 select * from tbl_musica;
